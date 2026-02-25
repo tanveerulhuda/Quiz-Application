2425f1e5bed2a774cb33f97cd5d9d2d9d0e361ad
tanveerilhuda70@gmail.com <tanveerulhuda70@gmail.com>
2026-02-25 23:50:13 +0530

    Quiz Application Project 1.1

--- QuizApp.java
+++ QuizApp.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
 
 public class QuizApp {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String questions [] = {
+   static String questions [] = {
             "Who invented Java",
             "Which key word is used to inherit a class",
             "which method is entry point of java"
         };
 
-        String options [][]={
+    static    String options [][]={
             {"1. james Gosling", "2. Dennis Ritchie", "3. Bjarne Stroustrup", "4. Guido van Rossum"},
             {"1. Implement", "2. Extends", "3. inherit", "4. super"},
             {"1. Start()", "2. run()", "3. main()", "4. init()"}
         };
 
-        int answer[]={1,2,3};
-        int score=0;
+    static int answer[]={1,2,3};
+    static int score=0;
+    static String correctAns = "";
+public static void main(String[] args) {
+    startQuiz();
+    showResult();
+    
+}    
 
-        //loop through all questions
+
+
+
+public static void startQuiz(){
+    Scanner sc = new Scanner(System.in);
+    //loop through all questions
         for(int i =0; i<questions.length;i++){
             System.out.println("nQuestion "+ (i+1));
             System.out.println(questions[i]);
@@ -27,7 +36,7 @@ public class QuizApp {
               //Print options
             for(int j = 0; j<options[i].length; j++){
                 System.out.println(options[i][j]);
-
+                correctAns=options[i][j];
             }
 
             System.out.println("Enter your answer (1-4): ");
@@ -39,16 +48,25 @@ public class QuizApp {
                 score++;
             } else{
                 System.out.println("wrong");
+                System.out.println("The correct answer is: "+ correctAns);
             }
-            
+}
+
+sc.close();
+}
+
+public static void showResult(){
+    double percentage =  (score *100) / questions.length;
+        System.out.println("\nYour Final Score: " + score + "/" + questions.length);
+        System.out.println("\nYour Percentage is: " + percentage+" %");
+
+        if (percentage>=50) {
+            System.out.println("Result: PASS");
+        }
+        else{
+            System.out.println("Result: FAIL");
         }
 
-        System.out.println("\nYour Final Score: " + score + "/" + questions.length);
-        sc.close();
-
-      
-    
-    
+}     
+   
 }
-}
-
